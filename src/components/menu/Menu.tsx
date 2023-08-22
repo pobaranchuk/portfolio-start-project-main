e5929d5a5d3775f8c6c401cf7e127a42a531af6f
pobaranchuk <you@example.com>
2023-08-22 15:19:53 +0200

    Next commit

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from "styled-components";
+
+export const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Skills</a>
+                </li>
+                <li>
+                    <a href="">Works</a>
+                </li>
+                <li>
+                    <a href="">Testimony</a>
+                </li>
+                <li>
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+const StyledMenu = styled.nav`
+    
+`
