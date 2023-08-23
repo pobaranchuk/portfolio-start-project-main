d3c0ed3e1425a13a4cf9c2f54fc17fbf501858c4
pobaranchuk <you@example.com>
2023-08-23 11:54:20 +0200

    Next commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills";
 import {Works} from "./layout/sections/works/Works";
+import {Testimony} from "./layout/sections/testimony/Testimony";
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
             <Main/>
             <Skills/>
             <Works/>
-
+            <Testimony/>
         </div>
     );
 }
