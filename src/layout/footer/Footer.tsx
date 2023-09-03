6804e7ddb0f55bba1cfd6fc5d0eb7bc5f7b6800b
pbaranchuk <pobaranchukv@gmail.com>
2023-09-03 19:29:47 +0200

    Footer sector styled

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -37,23 +38,45 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  background-color: #90c59c;
-  min-height: 30vh;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `
 const Name = styled.span`
-    
+  font-family: Josefin Sans, sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0; // сверху + снизу а по бокам 0
 `
 const SocialItem = styled.li`
     
     
 `
 const SocialLink = styled.a`
+  border-radius: 25px;
+  background-color: rgba(255, 255, 255, 0.10);
+  width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  color: ${theme.colors.accent}; // меняем поведения внутри свг иконки
+  
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    transform: translateY(-4px);
+  }
     
 `
 const Copyright = styled.small`
+  opacity: 0.5; // ибо цвет уже есть в боди, по этому мы только добавим прозрачность
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
     
 `
