51ef98e8f8dec4227cd0415190e0ccc1ba95d03f
pobaranchuk <you@example.com>
2023-09-07 10:11:53 +0200

    filter for section works added

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
-import {TabMenu} from "./tabMenu/TabMenu";
+import {TabMenu, TabsStatusType} from "./tabMenu/TabMenu";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
 import socialImg from ".//../../../assets/images/proj-1.webp"
 import timerImg from ".//../../../assets/images/proj-2.webp"
 import {Container} from "../../../components/Container";
 
-const worksItems = ["All", "Landing page", "React", "SPA"]
 
-const workData = [
+const tabsItems: Array<{ status: TabsStatusType, title: string }> = [
+    {
+        title: "All",
+        status: "all"
+    },
+    {
+        title: "Landing page",
+        status: "landing"
+    },
+    {
+        title: "React",
+        status: "react"
+    },
+    {
+        title: "SPA",
+        status: "spa"
+    }
+]
+
+const worksData = [
     {
         title: "Social Network",
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ",
-        src: socialImg
+        src: socialImg,
+        type: "spa"
     },
     {
         title: "Timer",
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit ut labore et dolore mgana alicua Ut enim",
-        src: timerImg
+        src: timerImg,
+        type: "react"
     }
 ]
 export const Works: React.FC = () => {
+    const [currentFilterStatus, setCurrentFilterStatus] = useState("all");
+    let filteredWorks = worksData;
+
+    if (currentFilterStatus === "landing") {
+        filteredWorks = worksData.filter(work => work.type === "landing")
+    }
+
+    if (currentFilterStatus === "react") {
+        filteredWorks = worksData.filter(work => work.type === "react")
+    }
+
+    if (currentFilterStatus === "spa") {
+        filteredWorks = worksData.filter(work => work.type === "spa")
+    }
+
+    function changeFilterStatus(value: TabsStatusType) {
+        setCurrentFilterStatus(value)
+    }
+
     return (
         <StyledWorks>
             <Container>
                 <SectionTitle>My Works</SectionTitle>
-                <TabMenu menuItems={worksItems}/>
+                <TabMenu tabsItems={tabsItems}
+                         changeFilterStatus={changeFilterStatus}
+                         currentFilterStatus = {currentFilterStatus}/>
                 <FlexWrapper justify={"space-between"} align={"flex-start"} wrap={"wrap"}>
-                    {workData.map((w, index) => {
+                    {filteredWorks.map((w, index) => {
                         return <Work title={w.title} key={index} text={w.text} src={w.src}/>
                     })}
                 </FlexWrapper>
--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import styled from "styled-components";
 import {Link} from "../../../../components/Link";
 
+export type TabsStatusType = "all" | "landing" | "react" | "spa";
 
-export const TabMenu = (props: { menuItems: Array<string> }) => {
+type TabMenuPropsType = {
+    tabsItems: Array<{ status: TabsStatusType, title: string }>,
+    changeFilterStatus: (value: TabsStatusType) => void,
+    currentFilterStatus: string
+}
+
+export const TabMenu = (props: TabMenuPropsType) => {// развёртываем обьект и говорим что в нём приходит
     return (
         <StyledTabMenu>
             <ul>
-                {props.menuItems.map((item, index) => {
+                {props.tabsItems.map((item, index) => {
                     return <ListItem key={index}>
-                        <Link href="#">{item}</Link>
+                        <Link active={props.currentFilterStatus === item.status} as={"button"} onClick={() => {
+                            props.changeFilterStatus(item.status)
+                        }}>{item.title}</Link>
                     </ListItem>
                 })}
             </ul>
