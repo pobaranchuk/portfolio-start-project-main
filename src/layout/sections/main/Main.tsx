fff1b32e5a84cfafa1c41a6a9c912bcaddc8ddfc
pobaranchuk <you@example.com>
2023-09-08 09:53:33 +0200

    Type writer effect added

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,6 +3,9 @@ import photo from './../../../assets/images/photo.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {S} from "./Main_Styles"
+import Typewriter from 'typewriter-effect';
+
+
 export const Main: React.FC = () => {
     return (
         <S.Main>
@@ -11,7 +14,17 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name> I am <span>Slava Pobaranchuk</span></S.Name>
-                        <S.MainTitle>A Web Developer</S.MainTitle>
+                        <S.MainTitle>
+                            <p>A Web Developer</p> {/*для выдачи в Гугле*/}
+                            <Typewriter
+                                options={{
+                                    strings: ['A Web Developer'],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 100
+                                }}
+                            />
+                        </S.MainTitle>
                     </div>
                     <S.PhotoWrapper>
                         <S.Photo src={photo} alt=""/>
--- src/layout/sections/main/Main_Styles.ts
+++ src/layout/sections/main/Main_Styles.ts
@@ -45,6 +45,10 @@ const Photo = styled.img`
 `
 const MainTitle = styled.h1`
   ${font({weight: 400, Fmax: 27, Fmin: 20})}
+
+  p{
+    display: none;
+  }
 `
 const Name = styled.h2`
   ${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
